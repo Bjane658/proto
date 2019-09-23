8078c19992c16fc137679cb44a943bc89f6b0ee7
Filip Krakowski <krakowski@hhu.de>
2019-09-23 12:16:38 +0200

    Rename project

--- apply.sh
+++ apply.sh
@@ -5,17 +5,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 docker run --rm \
     -u $(id -u ${USER}):$(id -g ${USER}) \
     -v "${DIR}":"/out" \
-    -v "${DIR}/codecamp":"/workspace/proto/codecamp" \
-    codecamp/protobuild:1.0 \
+    -v "${DIR}/educode":"/workspace/proto/educode" \
+    educode/protobuild:1.0 \
         /bin/bash -c "protoc -I /workspace/proto \
                              -I /workspace/protobuf/src \
                              --include_imports \
                              --include_source_info \
                              --descriptor_set_out=/out/proto.pb \
-                             /workspace/proto/codecamp/authenticator/api/v1.proto \
-                             /workspace/proto/codecamp/catalogue/api/v1.proto \
-                             /workspace/proto/codecamp/submission/api/v1.proto \
-                             /workspace/proto/codecamp/review/api/v1.proto"
+                             /workspace/proto/educode/authenticator/api/v1.proto \
+                             /workspace/proto/educode/catalogue/api/v1.proto \
+                             /workspace/proto/educode/submission/api/v1.proto \
+                             /workspace/proto/educode/review/api/v1.proto"
                         
-kubectl create configmap proto-descriptor --namespace codecamp --from-file "${DIR}/proto.pb" -o yaml --dry-run | kubectl apply -f -
+kubectl create configmap proto-descriptor --namespace educode --from-file "${DIR}/proto.pb" -o yaml --dry-run | kubectl apply -f -
 rm proto.pb
--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t codecamp/protobuild:1.0 "${DIR}"
+docker build -t educode/protobuild:1.0 "${DIR}"
 
 
